f9b859e5e74aa9146f7e24a19afbe23234d62a33
cjones1047 <cjmj1047@gmail.com>
2022-11-21 10:07:59 -0600

    Correctly runs report, turns off, or finds drink in menu based on user input

--- main.py
+++ main.py
@@ -37,3 +37,11 @@ is_on = True
 while is_on:
     options = menu.get_items()
     choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        print(drink)
