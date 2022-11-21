3421465eea35408b8a94df13d6952806aefb9bae
cjones1047 <cjmj1047@gmail.com>
2022-11-21 10:18:44 -0600

    Coffee machine correctly dispenses drink and debits resources from remaining machine resources if user's payment is sufficient and machines initial resources are sufficient.

--- main.py
+++ main.py
@@ -44,5 +44,5 @@ while is_on:
         money_machine.report()
     else:
         drink = menu.find_drink(choice)
-        if coffee_maker.is_resource_sufficient(drink):
-            print(money_machine.make_payment(drink.cost))
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
